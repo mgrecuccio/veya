406f67fb6a0918815325fa351eef1e9f298d178d
mgrecuccio <marco.grecuccio@mgrtech.eu>
2026-07-13 16:25:18 +0200

    Settings: updading user preferences API

--- src/app/core/api/services/user.service.ts
+++ src/app/core/api/services/user.service.ts
@@ -3,6 +3,10 @@ import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { UserPrivateProfileView } from "../model/user-private-profile-view.model";
+import { UpdateProfileRequest } from "../request/update-profile.request";
+import { UserMatchingPreferencesView } from "../model/user-matching-preferences-view.model";
+import { UpdatePreferencesRequest } from "../request/update-preferences.request";
+import { UserProfileView } from "../model/user-profile-view.model";
 
 
 @Injectable({ providedIn: 'root' })
@@ -12,6 +16,18 @@ export class UserService {
 
     getMe(): Observable<UserPrivateProfileView> {
         return this.http.get<UserPrivateProfileView>(`${this.apiBaseUrl}/api/v1/users/me`);
+    }
+
+    updateMe(payload: UpdateProfileRequest): Observable<UserProfileView> {
+        return this.http.put<UserPrivateProfileView>(`${this.apiBaseUrl}/api/v1/users/me`, payload);
+    }
+
+    getPreferences(): Observable<UserMatchingPreferencesView> {
+        return this.http.get<UserMatchingPreferencesView>(`${this.apiBaseUrl}/api/v1/users/preferences`);
+    }
+
+    updatePreferences(payload: UpdatePreferencesRequest): Observable<UserMatchingPreferencesView> {
+        return this.http.put<UserMatchingPreferencesView>(`${this.apiBaseUrl}/api/v1/users/preferences`, payload);
     }
 
 }
